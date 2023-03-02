9608fafc818e78cafe0aea16407a389c818ddae9
joaofrazaocorreia <joaopfrazaocorreia@gmail.com>
2023-03-02 17:40:16 +0000

    Add project TiposVarios. Create variables and print them.

---
+++ TiposVarios/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+
+namespace TiposVarios
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 8;
+            uint num2 = 4U;
+            long num3 = 7L;
+            ulong num4 = 2UL;
+
+            Console.WriteLine(num1+" "+num2+" "+num3+" "+num4);
+        }
+    }
+}
