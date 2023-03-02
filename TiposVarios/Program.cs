7c7f067d42a57c35925a7dc64464ade8c235b824
joaofrazaocorreia <joaopfrazaocorreia@gmail.com>
2023-03-02 17:57:50 +0000

    Add char variables and print them.

--- TiposVarios/Program.cs
+++ TiposVarios/Program.cs
@@ -9,12 +9,18 @@ namespace TiposVarios
     {
         static void Main(string[] args)
         {
+
             int num1 = 8;
             uint num2 = 4U;
             long num3 = 7L;
             ulong num4 = 2UL;
 
+            char musicNote = '\u266b';
+            char clover = '\u2663';
+            char sun = '\u263c';
+
             Console.WriteLine(num1+" "+num2+" "+num3+" "+num4);
+            Console.WriteLine(musicNote+" "+clover+" "+sun);
         }
     }
 }
