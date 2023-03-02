aee79d634e850371971f9c6b75a943d7556f9fcb
joaofrazaocorreia <joaopfrazaocorreia@gmail.com>
2023-03-02 18:26:42 +0000

    Add project VariasStrings. Create string variables and print them.

---
+++ VariasStrings/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace VariasStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string favAnimal= "My favorite animal are... \n ...sheep! \u263c";
+
+            string verbatimA= @"My favorite animal are... \n ... sheep! \u263c";
+
+            string paragraph= "\n ------------------------------- \t:D\n";
+
+            string verbParagraph= @"\n ------------------------------- \t:D\n";
+
+
+            Console.WriteLine(paragraph);
+            Console.WriteLine(favAnimal);
+            Console.WriteLine(verbatimA);
+            Console.WriteLine(verbParagraph);
+        }
+    }
+}
