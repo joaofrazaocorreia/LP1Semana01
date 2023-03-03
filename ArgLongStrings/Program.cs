fa08d6f70fecac9e125ff73212360a2116fe1df1
joaofrazaocorreia <joaopfrazaocorreia@gmail.com>
2023-03-03 13:31:33 +0000

    Add projects to solution. Create project ArgLongStrings.

---
+++ ArgLongStrings/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ArgLongStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i=0; i< args.Length ; i++)
+            {
+                if (args[i].Length>4)
+                {
+                    string uppercase = args[i].ToUpper();
+                    Console.WriteLine(uppercase);
+                }
+            }
+        }
+    }
+}
